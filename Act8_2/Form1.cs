ab8814430cce0963c8c88000b44f54dde43a9b33
Nemanja <nemadjo94@gmail.com>
2019-07-09 10:05:59 +0200

    Add files via upload

---
+++ Act8_2/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Act8_2
+{
+    public partial class frmLogger : Form
+    {
+        private delegate string AsyncReadLog(string filePath);
+        private AsyncReadLog LogReader = new AsyncReadLog(Logger.LogRead);
+
+        public void LogReadCallBack(IAsyncResult asyncResult)
+        {
+            MessageBox.Show(LogReader.EndInvoke(asyncResult));
+        }
+
+
+        public frmLogger()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogInfo_Click(object sender, EventArgs e)
+        {
+            this.Text = Logger.LogWrite(txtLogPath.Text, txtLogInfo.Text);
+        }
+
+        private void btnMessage_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
+
+        private void btnSyncRead_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Logger.LogRead(txtLogPath.Text));
+        }
+
+        private void btnAsyncRead_Click(object sender, EventArgs e)
+        {
+            AsyncCallback aCallBack = new AsyncCallback(LogReadCallBack);
+            IAsyncResult aResult = LogReader.BeginInvoke(txtLogPath.Text, aCallBack, null);
+        }
+    }
+}
---
+++ Act8_2/Logger.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace Act8_2
+{
+    class Logger
+    {   
+        //This will write log info into a txt file
+        public static string LogWrite(string logPath, string logInfo)
+        {
+            try
+            {
+                FileStream fileStream = new FileStream(logPath, FileMode.Open, FileAccess.Write);
+                StreamWriter streamWriter = new StreamWriter(fileStream);
+                fileStream.Seek(0, SeekOrigin.End);
+                streamWriter.WriteLine(DateTime.Now);
+                streamWriter.WriteLine(logInfo);
+                streamWriter.WriteLine();
+                streamWriter.Close();
+                return "Info Logged";
+            }
+            catch (FileNotFoundException exc)
+            {
+                return exc.Message;
+            }
+            catch (IOException exc)
+            {
+                return exc.Message;
+            }
+            catch
+            {
+                return "Logging Failed";
+            }
+        }
+        //This will read log info from txt file
+        public static string LogRead(string filePath)
+        {
+        StreamReader streamReader;
+        string fileText;
+
+           try
+           {
+                 streamReader = File.OpenText(filePath); //Load file into stream reader
+                  fileText = streamReader.ReadToEnd(); // read the file into string variable
+                 streamReader.Close(); //Close the stream reader
+                 Thread.Sleep(5000); //Simulate slow network connection
+                    return fileText;
+           }
+           catch(FileNotFoundException exc)
+           {
+                return exc.Message;
+           }
+            catch(IOException exc)
+            {
+                return exc.Message;
+            }
+            catch
+            {
+                return "Logging Failed";
+            }
+        }
+        
+    }
+}
